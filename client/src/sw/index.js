e9426d4761ce93c37903e07642f455e6a714cd72
pabloscript <pe.kozicki@gmail.com>
2019-01-13 16:06:31 +0100

    Enhance service worker

--- client/src/sw/index.js
+++ client/src/sw/index.js
@@ -1,3 +1,21 @@
 import '@babel/polyfill';
 
-console.warn('Hello, PWA!');
+const PRECACHE = 'static-v1';
+
+const PRECACHE_URLS = [
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  '/',
+  '/assets/icon_192x192.714a1246445a6a0da1e3aadf1451234a.png',
+  '/assets/manifest.json',
+  '/assets/index.js',
+];
+
+async function precache(urls) {
+  const cache = await caches.open(PRECACHE);
+  return cache.addAll(urls);
+}
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(precache(PRECACHE_URLS));
+});
+
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
+const fs = require('fs');
 const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
-const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
-const compress = require('koa-compress');
 const serveStatic = require('koa-static');
-const HTTP_PORT = process.env.PORT || 3000;
+const cors = require('@koa/cors');
+const compress = require('koa-compress');
+const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
 const router = new Router();
@@ -15,8 +16,15 @@ app.use(compress({
   threshold: 4096,
 }));
 app.use(bodyParser());
+
+const swSource = fs.readFileSync(path.join(__dirname, '..', 'dist', 'assets', 'sw.js'));
+router.get('/sw.js', async (ctx) => {
+  ctx.response.type = 'application/javascript';
+  ctx.response.body = swSource;
+});
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(serveStatic(path.join(__dirname, '..', 'dist')));
 
-app.listen(HTTP_PORT);
+app.listen(PORT);
